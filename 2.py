8c8187e0890d5555bfa13cf56b7f8e0562b32957
luowenbo <441592763@qq.com>
2020-09-14 12:30:06 +0800

    Frame(窗口/框架) demo

---
+++ 2.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+window = tk.Tk()  #创建顶级窗体对象
+window.title('zx108')  #设置window的标题
+window.geometry('300x200')   #设置窗体大小   x是字母
+window.resizable(height=False,width=True)
+window.mainloop()
