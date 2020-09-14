7405c5f9ca1f6d3e81daf5239e3ff5b70756153d
luowenbo <441592763@qq.com>
2020-09-14 21:45:38 +0800

    事件初体验

--- 7.py
+++ 7.py
@@ -1,3 +1,23 @@
 import tkinter as tk
 
+def callback():
+    print('单击了“显示”菜单！')
 
+
+
+
+window = tk.Tk()
+
+window.title('Menu 控件')
+
+window.geometry('400x400')
+
+menubar = tk.Menu(window)
+
+menubar.add_command(label='显示',command = callback)
+
+menubar.add_command(label='推出',command = window.quit)
+
+window.config(menu = menubar)
+
+window.mainloop()
